db108d936b6d0552e1ee6d0e25843cf3e0aa2892
RawatH <h.rawatdelhi@gmail.com>
2019-03-10 23:44:00 +0530

    Layouts

---
+++ RNTravels/ContactViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ContactViewController.swift
+//  RNTravels
+//
+//  Created by Harish on 03/03/19.
+//  Copyright © 2019 PACTS. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: BaseViewController , UITableViewDataSource , UITableViewDelegate {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initRnImgLabelView()
+      
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    private func initRnImgLabelView(){
+        
+    }
+
+}
